0f8de774e75793812b870616313d6b92eb527604
Aung Thu Hein <34760295+Aungthuhein187@users.noreply.github.com>
2020-05-18 23:08:06 +0630

    assignment_1

---
+++ assignment-1/src/main/java/com/jdc/assignment/entities/Course.java
@@ -0,0 +1,33 @@
+package com.jdc.assignment.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Course implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private int fees;
+	private double duration;
+	@Enumerated(EnumType.STRING)
+	private Level level;
+	
+	public enum Level {
+		Basic, Intermediate, Advance
+	}
+}
---
+++ assignment-1/src/main/java/com/jdc/assignment/entities/Teacher.java
@@ -0,0 +1,33 @@
+package com.jdc.assignment.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Teacher implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String phone;
+	private String email;
+	
+	@ManyToOne
+	private Address address;
+	
+	public Teacher() {
+		this.address = new Address();
+	}
+}
